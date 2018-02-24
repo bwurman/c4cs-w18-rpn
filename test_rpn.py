f2e5e10a5ba40c867f044840e458eb6c45152d12
bwurman <bwurman@verizon.net>
2018-02-24 17:31:56 -0500

    implemented add, subtract, multply, and divide

--- test_rpn.py
+++ test_rpn.py
@@ -5,4 +5,12 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
-
+    def test_subtract(self):
+        result = rpn.calculate("2 1 -")
+        self.assertEqual(1, result)
+    def test_multpily(self):
+        result = rpn.calculate("3 4 *")
+        self.assertEqual(12, result)
+    def test_divide(self):
+        result = rpn.calculate("9 3 /")
+        self.assertEqual(3.0, result)
